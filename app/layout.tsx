1caca02815b7a5c9adf50f81c6bb0c47bba057c2
thefuture-boss <jeyworking01@gmail.com>
2026-07-24 21:16:22 -0600

    Agregar Google AdSense

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 export const metadata: Metadata = {
@@ -13,9 +14,17 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es">
-      <body>
-        {children}
-      </body>
+      <head>
+        <Script
+          id="google-adsense"
+          async
+          strategy="afterInteractive"
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2347319059692192"
+          crossOrigin="anonymous"
+        />
+      </head>
+
+      <body>{children}</body>
     </html>
   );
 }
